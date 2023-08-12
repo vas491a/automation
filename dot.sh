021dc56855dcf1f2294f28cf587bede213b2751b
Anaparthi <srinivasanaparthi491@gmail.com>
2023-08-12 12:01:07 +0530

    addcommit

--- dot.sh
+++ dot.sh
@@ -2,5 +2,6 @@ sudo yum update -y
 sudo yum install java -y
 sudo yum install jenkins -y
 sudo service jenkins start
-sudo service status/systemctl service status
+sudo service status/systemctl servgitice status
 sudo systemctl stop service
+vi file name this is edit file local
